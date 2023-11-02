97dbc35f6771fda5a96e3f2390a14f199516cce8
Alvin <thisiseasytoremember1224@gmail.com>
2023-11-02 09:13:51 -0500

    example

--- quickChallenge/ViewController.swift
+++ quickChallenge/ViewController.swift
@@ -7,10 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+var names = ["one", "two", "three"]
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
         // Do any additional setup after loading the view.
     }
 
